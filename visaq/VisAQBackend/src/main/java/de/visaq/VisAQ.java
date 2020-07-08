db4a2f97f5b7661bd78b3f2a363f9182be18b96d
Daniel Schild <daniel.schild@student.kit.edu>
2020-07-08 12:34:50 +0200

    Set version to 0.0.2

--- visaq/VisAQBackend/src/main/java/de/visaq/VisAQ.java
+++ visaq/VisAQBackend/src/main/java/de/visaq/VisAQ.java
@@ -12,7 +12,7 @@ public class VisAQ {
     /**
      * Version of the server-software.
      */
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "0.0.2";
 
     /**
      * Spring application entry point.
